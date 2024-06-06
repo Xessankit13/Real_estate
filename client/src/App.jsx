ed81b5e1030b73ad5fb23dec0df67038e9eddc5b
Xessankit13 <132803543+Xessankit13@users.noreply.github.com>
2024-06-06 08:00:07 +0530

    Backend setup : conncted to MongoDB using mongoose , create a test api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Signin from './Pages/Signin'
 import Signup from './Pages/Signup'
 import About from './Pages/About'
 import Profile from './Pages/Profile'
+import Header from './Components/Header'
 
 const App = () => {
   return (
    <BrowserRouter>
+   <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/sign-in' element={<Signin/>}/>
