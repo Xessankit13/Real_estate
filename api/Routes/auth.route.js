c7035b576794257ca78c360707845ac5a842f6db
Xessankit13 <132803543+Xessankit13@users.noreply.github.com>
2024-06-07 07:45:02 +0530

    Sign-in Page

--- api/Routes/auth.route.js
+++ api/Routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import { signup } from '../Controlllers/auth.controller.js';
+import { signin, signup } from '../Controlllers/auth.controller.js';
 
 const router =express.Router();
 router.post('/signup',signup)
+router.post('/signin',signin)
 export default router ;
--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -1,11 +1,84 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
 
 const Signin = () => {
-  return (
-    <div>
-      signin
-    </div>
-  )
-}
+  const [formData, setformData] = useState({});
+  const [error, seterror] = useState(null);
+  const [loading, setloading] = useState(false);
+  const navigate = useNavigate();
 
-export default Signin
+  const handelChange = (e) => {
+    setformData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setloading(true);
+
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        seterror(data.message);
+        setloading(false);
+        return;
+      }
+      setloading(false);
+      seterror(null);
+      navigate('/');
+
+      console.log(data);
+    } catch (error) {
+      setloading(false);
+      seterror(error.message);
+    }
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+       
+        <input
+          onChange={handelChange}
+          type="text"
+          placeholder="email"
+          className="border p-3 rounded-lg "
+          id="email"
+        />
+        <input
+          onChange={handelChange}
+          type="text"
+          placeholder="password"
+          className="border p-3 rounded-lg "
+          id="password"
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500">{error}</p>}
+    </div>
+  );
+};
+
+export default Signin;
