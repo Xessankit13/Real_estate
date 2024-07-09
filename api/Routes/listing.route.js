8dfbbb5e50e1b437f4a6c424d4039a5434fae203
Xessankit13 <132803543+Xessankit13@users.noreply.github.com>
2024-07-09 10:30:40 +0530

    Delete user listing

--- api/Routes/listing.route.js
+++ api/Routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createListing } from '../Controlllers/listing.controller.js';
+import { createListing, deleteListing } from '../Controlllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
 const router = express.Router();
 router.post('/create',verifyToken,createListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
 
 export default router; 
