bd53e7a01dfe4c9bc3fa9000911387abb4b0ac2c
Xessankit13 <132803543+Xessankit13@users.noreply.github.com>
2024-07-09 13:42:30 +0530

    Update user listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import Signin from './Pages/Signin'
-import Signup from './Pages/Signup'
-import About from './Pages/About'
-import Profile from './Pages/Profile'
-import Header from './Components/Header'
-import PrivateRoute from './Components/PrivateRoute'
-import CreateListing from './Pages/CreateListing'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import Header from "./Components/Header";
+import PrivateRoute from "./Components/PrivateRoute";
+import CreateListing from "./Pages/CreateListing";
+import UpdateListing from "./Pages/UpdateListing";
 
 const App = () => {
   return (
-   <BrowserRouter>
-   <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/sign-in' element={<Signin/>}/>
-      <Route path='/sign-up' element={<Signup/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/create-listing' element={<CreateListing/>} />
-          </Route>
-    </Routes>
-   </BrowserRouter> 
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
